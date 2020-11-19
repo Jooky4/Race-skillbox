2450007e42813aabb579fad78affacb76d44663e
Izillger <44162880+Izillger@users.noreply.github.com>
2020-11-19 23:47:14 +0700

    Restart_2
    Зачистка для залития сцены с машинками и скриптами

---
+++ race2/Assets/Resources/Scripts/ScriptUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class ScriptUI : MonoBehaviour
+{
+    // public Text timerText = 3; //Текст таймера до старта гонок
+    
+    public void OnCklickStartGame() // Запуск игры
+    {
+        SceneManager.LoadScene("map1", LoadSceneMode.Additive);
+        //Application.LoadLevel(name: "map1"); // Запуск игровой сцены
+    }
+
+    public void OnCklickExitGame() // Выход из игры при нажатии кнопки в меню
+    {
+        Application.Quit();  // Закрыть приложение
+    }
+
+   /* public void TimerStart()
+    {
+
+    }*/
+    void Update() // Завершает работу приложения при нажатии на кнопку Esc
+    {
+        if (Input.GetKey("escape")) // если нажата кнопка Esc (Escape)
+        {
+            Application.Quit();  // Закрыть приложение
+        }
+    }
+}
