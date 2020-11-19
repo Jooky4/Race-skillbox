f8ee854d15f9d869ef0f237d82de28ae00a6a83c
rorchah1043 <romankiy@mail.ru>
2020-11-19 20:00:17 +0300

    Update Car2Controller.cs

--- race2/Assets/Scenes/Car2Controller.cs
+++ race2/Assets/Scenes/Car2Controller.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CarController : MonoBehaviour {
+public class Car2Controller : MonoBehaviour {
 
     public AxleInfo[] carAxis = new AxleInfo[2];
     public float carSpeed;
